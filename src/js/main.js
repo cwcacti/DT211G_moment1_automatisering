85cddf0402866bb5a2648281047b9a1ed3d535c9
Filip Nöbelin <filip.nobelin@gmail.com>
2024-01-16 16:31:37 +0100

    Introducera js
    Skapat en liten javascriptfil för att ha en simpel klocka på sidan
    Verkar inte ha fått jsfilen att laddas in ordentligt, att fixa

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,32 @@
 
 /* Variabler för redan existerande element */
+const mainDivElement = document.getElementById("maincontentdiv");
+
+/* Vid fönsterstart */
+window.onload(startUp);
+
+
+/* Uppstartsfunktion */
+function startUp() {
+    let newElement = document.createElement("article")
+    let newTextNode = document.createTextNode(clockMake())
+    mainDivElement.appendChild(newElement);
+    newElement.appendChild(newTextNode);
+    console.log("I did stuff");
+}
+
+/*Klockfunktion*/
+function clockMake() {
+    let todayDate = new Date();
+    let hours = todayDate.getHours();
+    let minutes = todayDate.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return hours + ":" + minutes;
+}
